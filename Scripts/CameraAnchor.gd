0a889b446b799dd63a267e282ef1a3ca86d7e07e
Caleb Hartshorn <calebmhartshorn@yahoo.com>
2020-05-07 19:28:11 -1000

    Door Animation/Sound Effects Added

--- Scripts/CameraAnchor.gd
+++ Scripts/CameraAnchor.gd
@@ -4,6 +4,9 @@ var smoothing = 0.2
 var offset = Vector2(0,0)
 var offset_width = 100
 var pos = Vector2()
+var target = Vector2()
+var max_scroll_speed = 32
+#ar vel = Vector2(0,0)
 
 var current_room = ""
 var map = preload("res://Map.tres")
@@ -20,8 +23,6 @@ func _physics_process(_delta):
 	
 	if not map.room_contains_position(get_node("../Player").position):
 		get_node("../Player").position = Vector2(256, 88)
-
-	
 	
 	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
 		if get_node("../Player/Sprite").flip_h:
@@ -29,16 +30,31 @@ func _physics_process(_delta):
 		else:
 			offset.x += (offset_width - offset.x) * 0.01
 	
-	pos.x += smoothing * ((get_node("../Player").get_position().x + offset.x) - pos.x)
-	pos.y += smoothing * ((get_node("../Player").get_position().y + offset.y) - pos.y)
+	var player_pos = get_node("../Player").get_position()
+	target = player_pos + offset
 	
-	var position_to_room = map.position_to_room(get_node("../Player").position)
+	var position_to_room = map.position_to_room(player_pos)
 	var room = map.map_data[position_to_room][0]
 	
-	pos.x = max(pos.x, room.position.x + (screen_size.x / 2))
-	pos.x = min(pos.x, room.position.x + room.size.x - (screen_size.x / 2))
-	pos.y = max(pos.y, room.position.y + (screen_size.y / 2))
-	pos.y = min(pos.y, room.position.y + room.size.y - (screen_size.y / 2))
+	target.x = max(target.x, room.position.x + (screen_size.x / 2))
+	target.x = min(target.x, room.position.x + room.size.x - (screen_size.x / 2))
+	target.y = max(target.y, room.position.y + (screen_size.y / 2))
+	target.y = min(target.y, room.position.y + room.size.y - (screen_size.y / 2))
+	
+	#sqt / (2.0f * (sqt - t) + 1.0f);
+	
+#	vel *= 0.8
+#	vel.x += 2 * ((target.x - pos.x) / abs(target.x - pos.y))
+#	vel.y += 2 * ((target.y - pos.y) / abs(target.y - pos.y))
+#	if abs(target.x - pos.x) < 1:
+#		vel.x *= abs(target.x - pos.x)
+#	if abs(target.y - pos.y) < 1:
+#		vel.y *= abs(target.y - pos.y)
+#
+#	pos += vel
+	
+	pos.x += to_range(smoothing * (target.x - pos.x), -max_scroll_speed, max_scroll_speed)
+	pos.y += to_range(smoothing * (target.y - pos.y), -max_scroll_speed, max_scroll_speed)
 	
 	position = Vector2(round(pos.x), round(pos.y))
 	
@@ -50,3 +66,6 @@ func _physics_process(_delta):
 				tiles.unload_room(i.get_name())
 		current_room = position_to_room
 		tiles.load_room(current_room, false)
+		
+func to_range(value, _min, _max):
+	return max(_min, min(value, _max))
---
+++ Scripts/Door_1.gd
@@ -0,0 +1,48 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var anim = get_node("AnimationPlayer")
+onready var open = get_node("open")
+onready var close = get_node("close")
+
+var map = preload("res://Map.tres")
+
+var dist = 64
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var player = get_tree().get_root().get_node("Root/Player")
+	var dif = player.position - position
+	if abs(dif.x) < dist and abs(dif.y) < dist:
+		anim.play("Open")
+	else:
+		anim.play("Closed")
+
+func _process(_delta):
+	var player = get_tree().get_root().get_node("Root/Player")
+	var dif = player.position - position
+	if abs(dif.x) < dist and abs(dif.y) < dist:
+		if flip_h:
+			if anim.get_assigned_animation() != "Closed->Open" and player.position.x < position.x and anim.get_assigned_animation() != "Open": #and map.position_to_room(player.position) == map.position_to_room(position):
+				anim.play("Closed->Open")
+				open.play()
+		else:
+			if anim.get_assigned_animation() != "Closed->Open" and player.position.x > position.x and anim.get_assigned_animation() != "Open": #and map.position_to_room(player.position) == map.position_to_room(position):
+				anim.play("Closed->Open")
+				open.play()
+	else:
+		if flip_h:
+			if anim.get_assigned_animation() != "Open->Closed" and player.position.x < position.x and anim.get_assigned_animation() != "Closed":#  and map.position_to_room(player.position) != map.position_to_room(position):
+				anim.play("Open->Closed")
+				close.play()
+		else:
+			if anim.get_assigned_animation() != "Open->Closed" and player.position.x > position.x and anim.get_assigned_animation() != "Closed":#  and map.position_to_room(player.position) != map.position_to_room(position):
+				anim.play("Open->Closed")
+				close.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
